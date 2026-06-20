9c495d43655c04df42634371d31286c34bfe32d6
Gustavo-LV-CE <ing.gustavolunav@gmail.com>
2026-06-20 10:51:26 -0600

    Fix spacing and add units to inspection report

--- inspection_report.py
+++ inspection_report.py
@@ -5,6 +5,6 @@ num_defects = 2
 inspector_name = "Gustavo L"
 print(f"Road name: {road_name}")
 print(f"Inspection date: {inspection_date}")
-print(f"Pavement thickness:{pavement_thickness}")
+print(f"Pavement thickness: {pavement_thickness} meters")
 print(f"Number of defects: {num_defects}")
 print(f"Inspector name: {inspector_name}") 
