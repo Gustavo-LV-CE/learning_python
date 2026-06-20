b8df336672fde65599965ecf59b8feaa0f3335b5
Gustavo-LV-CE <ing.gustavolunav@gmail.com>
2026-06-20 11:06:53 -0600

    Initial commit

---
+++ daily_siac_report_20_06_2026.py
@@ -0,0 +1,12 @@
+project_name = "Puente el Chical"
+date = "2026-06-20"
+num_workers = 2
+num_machines = 2
+num_samples = 3
+sample_thickness = 0.30
+print (f"Project name: {project_name}")
+print (f"Date: {date}")
+print (f"Number of workers: {num_workers}")
+print (f"number of machines: {num_machines}")
+print (f"number of samples: {num_samples}")
+print (f"Sample thickness: {sample_thickness} meters")
