701053a0797d0260ace6875ef82044b97836f8bd
Gustavo-LV-CE <ing.gustavolunav@gmail.com>
2026-06-20 11:51:31 -0600

    Learning control structures- if/else.

---
+++ spec_check.py
@@ -0,0 +1,5 @@
+compressive_strength = 195.24
+if compressive_strength >= 200:
+    print("The sample compressive strength reaches the minimum requirements.")
+else:
+    print("The sample compressive strength does not reach the minimum requirements.")
